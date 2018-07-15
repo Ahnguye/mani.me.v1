b8159255174f0b56554d8afae4102edc1465a814
Andre Nguyen <ahnguye@ucdavis.edu>
2018-07-14 21:05:23 -0700

    Dependencies

--- App.js
+++ App.js
@@ -4,13 +4,72 @@ import Amplify from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import { awsmobile } from './aws-exports';
 
+
+// Testing Functions
+import { API, Storage } from 'aws-amplify';
+import { Auth } from 'aws-amplify';
+
+import {
+  FormLabel,
+  FormInput,
+  FormValidationMessage,
+  Button,
+  Icon,
+  ButtonGroup,
+} from 'react-native-elements';
+
+
+
 Amplify.configure(awsmobile);
 
 export default class App extends React.Component {
+
+  state = {
+    input: {
+      name: ''
+    }
+  }
+  /*
+  Auth.signUp({
+    username,
+    password,
+    attributes: {
+        email,          // optional
+        phone_number,   // optional - E.164 number convention
+        // other custom attributes
+    },
+    validationData: []  //optional
+    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+  // After retrieveing the confirmation code from the user
+  Auth.confirmSignUp(username, code, {
+    // Optional. Force user confirmation irrespective of existing alias. By default set to True.
+    forceAliasCreation: true
+  }).then(data => console.log(data))
+  .catch(err => console.log(err));
+  */
+
+  updateInput = (name, nameValue) => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text></Text>
+        <FormInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            underlineColorAndroid="transparent"
+            editable={true}
+            placeholder="Please enter you pet's name"
+            returnKeyType="next"
+            ref="name"
+            textInputRef="nameInput"
+            onChangeText={(name) => this.updateInput('name', name)}
+            value={this.state.input.name}
+          />
       </View>
     );
   }
